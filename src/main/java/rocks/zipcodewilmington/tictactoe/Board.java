246115b15a97a9e301af95d7c608bafcb54b8291
benrybinski <benrybin@gmail.com>
2019-06-02 15:57:24 -0400

    all done

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,66 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
-    public Board(Character[][] matrix) {
+    String winner = "";
+
+    public  Board(Character[][] matrix) {
+        for (int i = 0; i < 3; i++) {
+
+            if (matrix[0][i].equals(matrix[1][i]) && matrix[0][i].equals(matrix[2][i])) {
+                if (matrix[0][i] != ' ') {
+                    winner = matrix[0][i].toString();
+                }
+            }
+            if (matrix[i][0].equals(matrix[i][1]) && matrix[i][0].equals(matrix[i][2])) {
+                if (matrix[i][0] != ' ') {
+                    winner = matrix[i][0].toString();
+                }
+            }
+            if (matrix[0][0].equals(matrix[1][1]) && matrix[0][0].equals(matrix[2][2])) {
+                if (matrix[0][0] != ' ') {
+                    winner = matrix[0][0].toString();
+                }
+
+            }
+            if (matrix[2][0].equals(matrix[1][1]) && matrix[2][0].equals(matrix[0][2])) {
+                if (matrix[2][0] != ' ') {
+                    winner = matrix[2][0].toString();
+                }
+            }
+
+        }
     }
 
+
     public Boolean isInFavorOfX() {
-        return null;
+        boolean answer = false;
+        if(winner.equals("X")){
+            answer = true;
+        }
+        return answer;
+
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        boolean answer = false;
+        if(winner.equals("O")){
+            answer = true;
+        }
+        return answer;
+
     }
 
     public Boolean isTie() {
-        return null;
+        boolean answer = false;
+        if(winner.equals("")){
+            answer = true;
+        }
+        return answer;
+
     }
 
     public String getWinner() {
-        return null;
+        return winner;
     }
 
 }
